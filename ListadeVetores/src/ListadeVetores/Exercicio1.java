75318b41e711f55c95f1e8eca233c2bb9f4cc7cc
Eduarda Dantas <78120410+arepmore@users.noreply.github.com>
2021-11-04 12:20:02 -0300

    Add files via upload

---
+++ ListadeVetores/src/ListadeVetores/Exercicio1.java
@@ -0,0 +1,27 @@
+package ListadeVetores;
+import java.util.Scanner;
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner (System.in);
+		final int TAM = 10;
+		int i, a[], b[];
+		a = new int [TAM];
+		b = new int [TAM];
+		
+		for (i=0; i<TAM; i++) {
+			System.out.println("Digite o " +(i+1)+ "o valor do vetor A");
+			a [i] = leia.nextInt();
+			b [i]= a[i]*a[i];
+		}	
+		System.out.print("a = [ ");
+		for (i=0; i<TAM; i++) {
+			System.out.print(b[i]+ " ");
+		}
+		System.out.print( "] ");
+	}
+}
+
+//Criar um programa que leia um vetor A com 10 posições de inteiro e imprima na
+//tela um vetor b sendo que cada elemento de B seja o quadrado de A
+
